ad62899037775d38e9a36ae61e6ff99199f8f6a4
kbl <m.pietraszek@gmail.com>
2017-12-12 14:19:26 +0100

    [OSP-4668]: limiting view to only owned entities

--- airflow/api/auth/backend/osp_auth.py
+++ airflow/api/auth/backend/osp_auth.py
@@ -4,6 +4,7 @@ from functools import wraps
 from flask import Response, request
 from airflow import models, settings
 
+from flask_admin.contrib.sqla.filters import FilterInList
 
 current_user = airflow.login.current_user
 dagbag = models.DagBag(settings.DAGS_FOLDER)
@@ -25,7 +26,8 @@ def osp_expose_only_owned_entities(function):
         if current_user.is_superuser():
             return function(*args, **kwargs)
 
-        dag_id = request.args.get('dag_id')
+        # Here I assume that url encoded filter for dag_id is under flt1
+        dag_id = request.args.get('dag_id') or request.args.get('flt1_dag_id_equals')
         dag = dagbag.get_dag(dag_id)
 
         if dag and dag.owner in current_user.osp_groups:
@@ -34,3 +36,15 @@ def osp_expose_only_owned_entities(function):
         return Response("Forbidden", 403)
 
     return decorated
+
+
+class DagOwnerInList(FilterInList):
+    def __init__(self):
+        column = 'owner'
+        name = 'osp_dag_owner'
+        options = None
+        data_type='select2-tags'
+        super(FilterInList, self).__init__(column, name, options, data_type)
+
+    def clean(self, value):
+        return current_user.osp_groups
