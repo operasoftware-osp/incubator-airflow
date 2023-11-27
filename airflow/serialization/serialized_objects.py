69880817333ddfaa223fb1bf8c01fded8329b60f
Tomasz Lecki <tlecki@opera.com>
2023-11-27 14:36:58 +0100

    Quickfix: Deserialize unregistered links too

--- airflow/serialization/serialized_objects.py
+++ airflow/serialization/serialized_objects.py
@@ -1563,8 +1563,7 @@ class SerializedBaseOperator(BaseOperator, BaseSerialization):
                     _operator_link_class_path
                 ]
             else:
-                log.error("Operator Link class %r not registered", _operator_link_class_path)
-                return {}
+                single_op_link_class = import_string(_operator_link_class_path)
 
             op_link_parameters = {param: cls.deserialize(value) for param, value in data.items()}
             op_predefined_extra_link: BaseOperatorLink = single_op_link_class(**op_link_parameters)
