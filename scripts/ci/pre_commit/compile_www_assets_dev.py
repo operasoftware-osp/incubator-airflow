96b492398b7cc1838a12fe8c8047e44263f4c10e
Łukasz Arczyński <larczynski@opera.com>
2025-12-26 13:59:28 +0100

    Quickfix: Use npx in compile_assets.sh

--- scripts/ci/pre_commit/compile_www_assets_dev.py
+++ scripts/ci/pre_commit/compile_www_assets_dev.py
@@ -49,14 +49,14 @@ if __name__ == "__main__":
     WWW_ASSET_OUT_FILE.unlink(missing_ok=True)
     with open(WWW_ASSET_OUT_DEV_MODE_FILE, "w") as f:
         subprocess.run(
-            ["yarn", "install", "--frozen-lockfile"],
+            ["npx", "yarn", "install", "--frozen-lockfile"],
             cwd=os.fspath(www_directory),
             check=True,
             stdout=f,
             stderr=subprocess.STDOUT,
         )
         subprocess.run(
-            ["yarn", "dev"],
+            ["npx", "yarn", "dev"],
             check=True,
             cwd=os.fspath(www_directory),
             env=env,
